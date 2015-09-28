31da97f237251227d3d4a5ca583f8e86f5136a0f
Sven A. Schmidt <sas@feinstruktur.com>
2015-09-28 11:28:15 +0100

    Initial Commit

---
+++ HastyHare/HastyHare.h
@@ -0,0 +1,19 @@
+//
+//  HastyHare.h
+//  HastyHare
+//
+//  Created by Sven A. Schmidt on 28/09/2015.
+//  Copyright © 2015 feinstruktur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HastyHare.
+FOUNDATION_EXPORT double HastyHareVersionNumber;
+
+//! Project version string for HastyHare.
+FOUNDATION_EXPORT const unsigned char HastyHareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HastyHare/PublicHeader.h>
+
+
